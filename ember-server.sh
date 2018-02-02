b650ffebc20fdf4ac517692cc3cbc6c96f0be9cc
Martin Höger <martin.hoger@gmail.com>
2018-02-02 10:59:34 +0100

    Pass params from URL

--- ember-server.sh
+++ ember-server.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-# docker run -it --rm -v "$PWD":/data mhoger/ember bash
+# Possible to start the server and pass the API host by env variable:
+# sudo API_HOST="https://aaaa.cz" ./bin/ember-server.sh
+if [[ "$API_HOST" != "" ]]; then
+    echo "API host: $API_HOST"
+fi
 
 # Remove running container
 docker rm -f "ember" &> /dev/null
@@ -13,6 +16,7 @@ docker run \
     -v "$PWD":/data \
     -p 4200:4200 \
     -p 49152:49152 \
+    -e "API_HOST=$API_HOST" \
     --name="ember" \
     mhoger/ember \
     ember server --port 4200 --live-reload-port 49152
