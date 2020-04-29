bdf817a8affb1eece863f7fb0f897e66aff35c7f
Martin Höger <martin.hoger@gmail.com>
2020-04-29 11:50:31 +0200

    Sudo added

--- ember-install-packages.sh
+++ ember-install-packages.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-docker run -it --rm -v "$PWD":/data mhoger/ember bower install --allow-root 
+sudo docker run -it --rm -v "$PWD":/data mhoger/ember bower install --allow-root 
 echo "NPM"
-docker run -it --rm -v "$PWD":/data mhoger/ember npm install
+sudo docker run -it --rm -v "$PWD":/data mhoger/ember npm install
