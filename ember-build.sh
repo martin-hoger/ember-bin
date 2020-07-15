f014703c953a902b60dfc75a46ce5adac2cf7f7e
Martin Höger <martin.hoger@gmail.com>
2020-07-15 15:22:50 +0200

    Build changed

--- ember-build.sh
+++ ember-build.sh
@@ -4,14 +4,24 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
 ENV_NAME=$1
+# ENV_NAME="development"
 if [[ "$ENV_NAME" == "" ]]; then
     echo "Error: you must specify environment name"
     echo "Ex: production"
     exit 1
 fi
 
-docker run \
+sudo docker run \
     -it --rm \
     -v "$PWD":/data \
     mhoger/ember \
-    ember build --environment="$ENV_NAME"
+    node --max_old_space_size=999999 /usr/local/bin/ember build --environment="$ENV_NAME"
+
+# MH: 
+#
+# https://stackoverflow.com/questions/37509159/ember-js-pass-options-to-node-js
+#
+# ember build --environment="$ENV_NAME"
+# changed to:
+# node --max_old_space_size=999999 /usr/local/bin/ember build --environment="$ENV_NAME"
+
