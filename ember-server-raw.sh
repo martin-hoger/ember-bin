465829e0c14f5c72a4a23e62784ddc628d07c328
Martin Höger <martin.hoger@gmail.com>
2021-12-02 14:43:17 +0100

    Run with paramters

---
+++ ember-server-raw.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+# Possible to start the server and pass the API host by env variable:
+# sudo API_HOST="https://aaaa.cz" ./bin/ember-server.sh
+if [[ "$API_HOST" != "" ]]; then
+    echo "API host: $API_HOST"
+fi
+
+# Remove running container
+docker rm -f "ember" &> /dev/null
+
+docker run \
+    -it --rm \
+    -v "$PWD":/data \
+    -p 4200:4200 \
+    -p 49152:49152 \
+    -e "API_HOST=$API_HOST" \
+    --name="ember" \
+    mhoger/ember \
+    node --max_old_space_size=999999 /data/node_modules/.bin/ember server --port 4200 --live-reload-port 49152
