30ee4732917f554af1b19f1e12a250713699ec35
Martin Höger <martin.hoger@gmail.com>
2024-09-04 10:32:54 +0200

    Install also npm

--- ember-reset-packages.sh
+++ ember-reset-packages.sh
@@ -19,8 +19,8 @@ $SUDO_PREFIX docker run -it \
     rm -rf node_modules bower_components tmp
     chmod 777 /root -R
     chown node:node /root -R
-    bower install --allow-root 
+    bower install --allow-root
     yarn install
+    npm install
 "
-    # npm install
 # ./bin/ember-rebuild-node-sass.sh
